65673e04a68288e18a397ef5379608181b386c7e
Adam Varga <adam@vrg.hu>
2020-05-30 12:30:24 +0200

    Fix underTenThousand conversion logic

--- js-numerals/src/number-conversion/index.js
+++ js-numerals/src/number-conversion/index.js
@@ -37,7 +37,7 @@ function underTenThousand(N) {
         namedPowersOfTen[3]
       }`;
     } else {
-      result += `${baseNumbers[Math.floor(N / 100)]} ${namedPowersOfTen[2]}`;
+      result += `${underHundred(Math.floor(N / 100))} ${namedPowersOfTen[2]}`;
     }
   }
 
@@ -50,7 +50,7 @@ function underTenThousand(N) {
 
 export default function convertNumber(N) {
   if (N >= Math.pow(10, 21)) {
-    throw new Error('Too large number: N < 10^21');
+    throw new Error("Too large number: N < 10^21");
   }
 
   if (!N) {
@@ -58,26 +58,29 @@ export default function convertNumber(N) {
   }
 
   if (N < 10000) {
-    return underTenThousand(N)
+    return underTenThousand(N);
   } else {
-    let result = '';
+    let result = "";
     let remainder = N;
 
-    const highestNamedPowerOfTen = Math.log10(N) - Math.log10(N) % 3;
-
+    const highestNamedPowerOfTen = Math.log10(N) - (Math.log10(N) % 3);
 
     for (let i = highestNamedPowerOfTen; i > 0; i -= 3) {
-      const M = Math.floor(remainder / Math.pow(10, i))
+      const M = Math.floor(remainder / Math.pow(10, i));
 
       if (M) {
-        result += `${result.length ? ' ' : ''}${underThousand(M)} ${namedPowersOfTen[i]}`;
+        result += `${result.length ? " " : ""}${underThousand(M)} ${
+          namedPowersOfTen[i]
+        }`;
 
         remainder -= M * Math.pow(10, i);
       }
     }
 
     if (remainder) {
-        result += `${remainder < 100 ? ' and ' : ' '}${underTenThousand(remainder)}`;
+      result += `${remainder < 100 ? " and " : " "}${underTenThousand(
+        remainder
+      )}`;
     }
 
     return result;
